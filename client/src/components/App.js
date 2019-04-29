c99ab9973aa927efac41c15ed2cdb9b8d33245da
Andrey Moroz <drew.vfrost@gmail.com>
2019-04-29 22:51:18 +0300

    react setup

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,7 +1,15 @@
 import React, { Component } from "react";
 
 export default class App extends Component {
+  state = { walletInfo: { address: "test adress", balance: 2282 } };
   render() {
-    return <div>Welcome nahuy</div>;
+    const { address, balance } = this.state.walletInfo;
+    return (
+      <div>
+        <div>Welcome</div>
+        <div>Addres: {address}</div>
+        <div>Balance: {balance}</div>
+      </div>
+    );
   }
 }
